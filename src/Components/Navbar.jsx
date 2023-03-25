039a962f313f9e7c58012be6dbf7092eb5c43040
ahmetcetnn <ahmetlaw@outlook.com>
2023-03-26 02:24:26 +0300

    homepage created

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    // Header Section
+    <>
+    <header className="bg-black py-3 pl-10 lg:py-6 text-gray-400 font-serif uppercase">
+      {/* Header Container */}
+    <div className="container flex items-center justify-between space-x-8 lg:space-x-16 ">
+      {/* LOGO */}
+      <h1 className=" lg:text-4xl text-3x1 font-bold text-gray-400 text-transparent bg-gradient-to-r bg-clip-text from-slate-500 to-slate-200">yekFit</h1>
+     
+      {/* Navigation */}
+      <nav className="flex justify-between flex-1">
+        {/* MENU */}
+        <div className="hidden md:flex py-6 items-start text-xs lg:text-base space-x-4 lg:space-x-8">
+          <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">exercise</a>
+          <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">nutrition</a>
+          <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">Progress</a>
+          <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">Blog</a>
+          <a className="hover:text-blue-600 hover:cursor-pointer transition duration-100">EXIT</a>
+          <form>
+          <div className="border-r px-4  py-1 border-red-400">
+            <input type="text" 
+            className="bg-transparent border-b border-red-500 w-24 lg:w-44 focus:outline-none" 
+            placeholder="Search.."></input>
+            <button className="font-semibold"><i className="fas fa-search"/></button>
+          </div>
+
+        </form>
+        </div>
+        {/* Login Area */}
+        <div className="flex items-center space-x-4 lg:space-x-8">
+        {/* Search Area */}
+        
+        {/* Signup Area */}
+        <div className="hidden md:text-xs md:flex items-center space-x-4 lg:space-x-8 lg:text-base">
+          <a className="cursor-pointer md:text-sm">Login</a>
+          <a className="bg-red-600 px-3 py-1 hover:bg-rose-600 hover:text-gray-600 cursor-pointer transition duration-100 rounded-sm whitespace-nowrap">Sign Up</a>
+        </div>
+        </div>
+      </nav>
+       {/* Mobile Menu */}
+       <div className="block md:hidden mr-14">
+        <div className="space-y-1 cursor-pointer">
+          <div className="bg-slate-500 w-8 h-1 rounded-full"></div>
+          <div className="bg-slate-500 w-8 h-1 rounded-full"></div>
+          <div className="bg-slate-500 w-8 h-1 rounded-full"></div>
+        </div>
+      </div>
+    </div>
+    
+
+
+
+    </header>
+   
+   
+
+
+
+
+    </>
+  );
+}
+
+export default Navbar;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,23 +1,188 @@
 import React from "react";
-
+import Navbar from "../Components/Navbar";
 function Homepage() {
   return (
-    <div className=" mt-10 flex items-center ">
-        <div className="w-1/3 text-black mt-32 ml-52">
-            <h1 className="text-6xl">Start Your </h1> 
-            <h1 className="text-6xl">New Life</h1>
-            <p className="text-sm mt-8 ">"Fitness is not about being better than someone else. It's about being better than you used to be."</p>
-            <button className="mt-8 mr-1 text-white border-solid bg-slate-600 w-36 rounded-sm">Get Started</button>
-            <button className="ml-1 mt-8 text-white border-solid bg-slate-600 w-36 rounded-sm">Preview</button>
-        </div>
-      <div className="h-80 mt-36 ml-24 ">
-        <img 
+    <>
+    <Navbar/>
+    <section className="h-64 md:h-96  lg:h-128 group relative">
+        {/* Hero Image */}
+        <img
           src="https://blog.n11.com/wp-content/uploads/2015/09/fitness-nedir-ne-ise-yarar-800x461.jpg"
-          alt="homepagephoto"
-          className=" w-72 lg:w-128 rounded-xl "
+          alt="main photo"
+          className="h-full w-full object-cover"
         />
-      </div>
-    </div>
+
+        {/* Hero Content */}
+        <div className="absolute bottom-0 w-full bg-gradient-to-b from-transparent to-black">
+          {/* Hero content container */}
+          <div className="container">
+            
+            <h1 className=" text-3xl lg:text-5xl text-white group-hover:mb-1 duration-500 ">
+              START YOUR NEW LIFE
+            </h1>
+            <p className="text-white group-hover:mb-2 duration-500 w-3/4 text-sm lg:text-base lg:w-2/3">
+            "Fitness is not about being better than someone else. It's about
+            being better than you used to be."
+            </p>
+            {/* detail container */}
+            <div className="flex space-x-8 opacity-0 group-hover:opacity-100 group-hover:mb-5 lg:group-hover:mb-15 duration-1000 ">
+              {/* watch */}
+              
+              {/* INFO */}
+              <div className="flex space-x-2 items-center cursor-pointer ">
+                <button className="text-gray-100 uppercase w-32 border-solid bg-opacity-5 md:text-xs lg:text-lg hover:text-red-600 duratin-500">
+                  start now
+                </button>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Movies section */}
+      <section className="py-10 bg-black  ">
+        {/* Movies content */}
+        <div className=" flex ml-10 space-x-8">
+          {/* sol taraf */}
+          {/* titles */}
+
+          <div className=" basis-2/3 ">
+            {/* images container */}
+            <div className="flex flex-wrap ">
+              {/* image 1 */}
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="https://iphoneswallpapers.com/wp-content/uploads/2018/07/Aquaman-HD-iPhone-Wallpaper.jpg"
+                  alt="logo"
+                  className="group-hover:scale-110 group:hover:opacity-50"
+                />
+                {/* image detail container */}
+
+                <div className="absolute px-6 bottom-8">
+                  <h3 className="text-gray-200 group-hover:text-gray-400 group-hover:mb-2">
+                    AQUAMAN
+                  </h3>
+                  <p className="text-gray-400 text-xs opacity-0 group-hover:opacity-100 grouo-hover:mb-10 duration-500 ">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quae, sunt.
+                  </p>
+                </div>
+              </div>
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="https://wallpapercave.com/wp/wp6875764.jpg"
+                  alt="logo"
+                  className="group-hover:scale-110 group:hover:opacity-50"
+                />
+                {/* image detail container */}
+
+                <div className="absolute px-6 bottom-8">
+                  <h3 className="text-gray-200 group-hover:text-gray-400 group-hover:mb-2">
+                    FLASH
+                  </h3>
+                  <p className="text-gray-400 text-xs opacity-0 group-hover:opacity-100 grouo-hover:mb-10 duration-500 ">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quae, sunt.
+                  </p>
+                </div>
+              </div>
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="https://w0.peakpx.com/wallpaper/729/654/HD-wallpaper-hugh-jackman-comics-logan-marvel-mutant-wolverine-x-men-thumbnail.jpg"
+                  alt="logo"
+                  className="group-hover:scale-110 group:hover:opacity-50"
+                />
+                {/* image detail container */}
+
+                <div className="absolute px-6 bottom-8">
+                  <h3 className="text-gray-200 group-hover:text-gray-400 group-hover:mb-2">
+                    WOLVERINE
+                  </h3>
+                  <p className="text-gray-400 text-xs opacity-0 group-hover:opacity-100 grouo-hover:mb-10 duration-500 ">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quae, sunt.
+                  </p>
+                </div>
+              </div>
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="https://w0.peakpx.com/wallpaper/959/836/HD-wallpaper-captain-america-avenger-shield-steve-rogers.jpg"
+                  alt="logo"
+                  className="group-hover:scale-110 group:hover:opacity-50"
+                />
+                {/* image detail container */}
+
+                <div className="absolute px-6 bottom-8">
+                  <h3 className="text-gray-200 group-hover:text-gray-400 group-hover:mb-2">
+                    CAPTAIN AMERICA
+                  </h3>
+                  <p className="text-gray-400 text-xs opacity-0 group-hover:opacity-100 grouo-hover:mb-10 duration-500 ">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quae, sunt.
+                  </p>
+                </div>
+              </div>
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="https://r1.ilikewallpaper.net/iphone-wallpapers/download/85429/spiderman-2002-iphone-wallpaper-ilikewallpaper_com.jpg"
+                  alt="logo"
+                  className="group-hover:scale-110 group:hover:opacity-50 "
+                />
+                {/* image detail container */}
+
+                <div className="absolute px-6 bottom-8">
+                  <h3 className="text-gray-200 group-hover:text-gray-400 group-hover:mb-2">
+                    SPIDERMAN
+                  </h3>
+                  <p className="text-gray-400 text-xs opacity-0 group-hover:opacity-100 grouo-hover:mb-10 duration-500 ">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quae, sunt.
+                  </p>
+                </div>
+              </div>
+              <div className="group relative overflow-hidden basis-1/3">
+                <img
+                  src="https://r1.ilikewallpaper.net/iphone-wallpapers/download-108622/justice-league-zack-superman-black-suit-4k.jpg"
+                  alt="logo"
+                  className="group-hover:scale-110 duration-200 group:hover:opacity-50"
+                />
+                {/* image detail container */}
+
+                <div className="absolute px-6 bottom-8">
+                  <h3 className="text-gray-200 group-hover:text-gray-400 group-hover:mb-2">
+                    SUPERMAN
+                  </h3>
+                  <p className="text-gray-400 text-xs opacity-0 group-hover:opacity-100 grouo-hover:mb-10 duration-500 ">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Quae, sunt.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* sag taraf */}
+          <div className="basis1/3 text-slate-400 flex flex-col">
+            <div className="h-72 my-72">
+            <h2 className="lg:text-3xl md:text-xl sm:text-base text-center justify-center  whitespace-pre-wrap ">
+              You won't find an unhealthy, 
+            </h2>
+            <h2 className="lg:text-3xl md:text-xl sm:text-base text-center justify-center whitespace-pre-wrap ">
+            skinny superhero in any comic book.
+            </h2>
+            <h1 className="lg:text-6xl md:text-3xl sm:text-lg text-center  items-center justify-center ">
+              Be Your Own Hero!
+            </h1>
+            <h2 className="lg:text-3xl md:text-xl sm:text-base text-center justify-center  whitespace-pre-wrap ">
+            You can use your power to rescue kittens from a tree
+            </h2>
+            <h2 className="lg:text-3xl md:text-xl sm:text-base text-center justify-center whitespace-pre-wrap ">
+            NOT TO STOP BULLETS!
+            </h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
