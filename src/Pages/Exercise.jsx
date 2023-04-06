eb0b3748d54903ccef7cde2f8d8d503ca011fda8
ahmetcetnn <ahmetlaw@outlook.com>
2023-04-06 22:15:14 +0300

    <

--- src/Pages/Exercise.jsx
+++ src/Pages/Exercise.jsx
@@ -25,12 +25,21 @@ function Exercise() {
   return (
     <>
     <Navbar/>
+    <div className='flex flex-row gap-4 w-full'>
+      <button className='w-48 rounded-md h-5 text-black bg-cyan-600'>Shoulder</button>
+      <button className='w-48 rounded-md h-5 text-black bg-cyan-600'>Arms</button>
+      <button className='w-48 rounded-md h-5 text-black bg-cyan-600'>Chest</button>
+      <button className='w-48 rounded-md h-5 text-black bg-cyan-600'>Back</button>
+      <button className='w-48 rounded-md h-5 text-black bg-cyan-600'>Legs</button>
+      <button className='w-48 rounded-md h-5 text-black bg-cyan-600'>Core</button>
+      <button className='w-48 rounded-md h-5 text-black bg-cyan-600'>Cardio</button>
+    </div>
     <div className="container ">
 
     <div className='container grid lg:grid-cols-3 lg:gap-7 grid-cols-1 md:items-center  justify-center'>
         {exerciseList.map(exercise => (
           <div className='container grid lg:grid-cols-3 lg:gap-7  md:items-center text-white justify-center' key={exercise.id}>
-            <h4 className='text-white'>{exercise.name}</h4>
+            <h4 className='text-white uppercase'>{exercise.name}</h4>
             <img className='container lg:w-32 w-20 rounded-md  items-center justify-center' src={exercise.gifUrl} alt={exercise.bodyPart}/>
             </div>
         ))}
