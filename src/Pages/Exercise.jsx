d47caccdcb133fb838a9dbf35aa2ad851c23665c
ahmetcetnn <ahmetlaw@outlook.com>
2023-04-06 20:50:48 +0300

    <

--- src/Pages/Exercise.jsx
+++ src/Pages/Exercise.jsx
@@ -5,14 +5,28 @@ import { useState,useEffect } from 'react'
 
 
 function Exercise() {
- const fetchData = async(url,options)=> {
-  const response =await fetch(url,options);
-  const data=await response.json();
-  return data;
-}
   
+  const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+  const fetchData = async(url,exerciseOptions) => {
+    const response = await fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',exerciseOptions);
+    const data = await response.json();
+    const url= "https://exercisedb.p.rapidapi.com/exercises/bodyPartList";
+    return data;
+  }
 
-  
+   
  
 
 
