9fe3724ea5a15c3828bc6e5e969547c5c5e302a7
ahmetcetnn <ahmetlaw@outlook.com>
2023-04-13 14:53:00 +0300

    <

--- src/Pages/Links/Arms.jsx
+++ src/Pages/Links/Arms.jsx
@@ -9,7 +9,7 @@ function Arms() {
   const options = {
     method: 'GET',
         headers: {
-      'X-RapidAPI-Key': '4873c1c780mshc01834047e55702p1b8219jsnd79dd09da5c9',
+      'X-RapidAPI-Key': 'aebe601bd9mshd5747766cf04ca7p1db892jsne8b8273941ef',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
