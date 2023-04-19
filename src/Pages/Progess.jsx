bbe3d742cff9b3f803881c38e7e44e64c56ece5b
ahmetcetnn <ahmetlaw@outlook.com>
2023-04-19 16:07:01 +0300

    <

--- src/Pages/Progess.jsx
+++ src/Pages/Progess.jsx
@@ -13,12 +13,12 @@ function Progess() {
       .catch(err => console.error(err));
       
   }, []);
-  const [selectedExercise, setSelectedExercise] = useState(null);
+  const [selectedExercise, setSelectedExercise] = useState([]);
 
   const handleSelectExercise = (event) => {
     const exerciseId = event.target.value;
     const exercise = exerciseList.find((exercise) => exercise.id === exerciseId);
-    setSelectedExercise((prevSelectedExercises) => [...prevSelectedExercises, exercise]);
+    setSelectedExercise(exercise);
   };
 
   return (
@@ -27,14 +27,14 @@ function Progess() {
     <div className="container relative mt-8 grid lg:grid-cols-3 lg:gap-7 grid-cols-1 text-center items-center justify-center">
   <div className="container relative grid lg:grid-cols-3 lg:gap-7 items-center text-center md:items-center text-black justify-center">
     <div className="text-center items-center uppercase w-48">
-      <p>Select an exercise</p>
+      <p className='text-white text-center uppercase items-center'>Select an exercise</p>
       <select className="text-center items-center justify-center text-black w-full" onChange={handleSelectExercise}>
-        <option className='text-center items-center uppercase' value="">Select An Exercise</option>
+        <option className='text-center items-center text-white uppercase' value="">Select </option>
         {exerciseList.map((exercise) => (
           <option className='uppercase' key={exercise.id} value={exercise.id}>
             {exercise.name}
           </option>
-        ))}
+        ))}n
       </select>
       {selectedExercise && (
         <>
@@ -45,13 +45,20 @@ function Progess() {
               <option className='w-36 text-center items-center' value="5kg">5kg</option>
               <option className='w-36 text-center items-center' value="8kg">8kg</option>
               <option className='w-36 text-center items-center' value="10kg">10kg</option>
+              <option className='w-36 text-center items-center' value="12,5kg">12,5kg</option>
               <option className='w-36 text-center items-center' value="15kg">15kg</option>
+              <option className='w-36 text-center items-center' value="17,5kg">17,5kg</option>
               <option className='w-36 text-center items-center' value="20kg">20kg</option>
+              <option className='w-36 text-center items-center' value="22,5kg">22,5kg</option>
               <option className='w-36 text-center items-center' value="25kg">25kg</option>
               <option className='w-36 text-center items-center' value="30kg">30kg</option>
               <option className='w-36 text-center items-center' value="35kg">35kg</option>
               <option className='w-36 text-center items-center' value="40kg">40kg</option>
               <option className='w-36 text-center items-center' value="45kg">45kg</option>
+              <option className='w-36 text-center items-center' value="50kg">50kg</option>
+              <option className='w-36 text-center items-center' value="55kg">55kg</option>
+              <option className='w-36 text-center items-center' value="60kg">60kg</option>
+              <option className='w-36 text-center items-center' value="65kg">65kg</option>
             </select>
           </div>
         </>
