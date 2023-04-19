776d9d99256e539842c99a278e892a8a2f6fd00f
ahmetcetnn <ahmetlaw@outlook.com>
2023-04-19 16:56:48 +0300

    <

--- src/Pages/Links/Chest.jsx
+++ src/Pages/Links/Chest.jsx
@@ -26,22 +26,24 @@ function Chest() {
     <>
     <Navbar/>
     <Routers/>
-    <div className="container ">
-
-  <div className='container relative mt-8 grid lg:grid-cols-3 lg:gap-7 grid-cols-1 md:items-center  justify-center'>
-      {exerciseList.map(exercise => (
-        <div className='container relative grid lg:grid-cols-3 lg:gap-7  md:items-center text-white justify-center' key={exercise.id}>
-        <h4 className='text-white text-center items-center uppercase'>{exercise.name}</h4>
-        <img className='container block opacity-100 transition-all  hover:opacity-30 lg:w-32 w-52 rounded-md   items-center justify-center' src={exercise.gifUrl} alt={exercise.bodyPart}/>
-   
-   
-        <div className='hover:opacity-100 absolute top-1/2 left-1/2 opacity-0 text-center items-center  -translate-x-1/2 -translate-y-1/2'> 
-          <button className='text-white w-28 rounded-sm border-solid ml-6 bg-slate-600 whitespace-nowrap opacity-80 'onClick={() => addMoveToMovelist(exercise)}>ADD MOVE </button> 
-          </div>
+    <div className="container">
+      {exerciseList.length === 0 ? ( // exerciseList verisi boş ise "Moves are loading" mesajını göster
+        <div className="text-white text-center">Loading..</div>
+      ) : (
+        // exerciseList verisi dolu ise verileri haritalayarak bileşenleri oluştur
+        <div className='container relative mt-8 grid lg:grid-cols-3 lg:gap-7 grid-cols-1 md:items-center justify-center'>
+          {exerciseList.map(exercise => (
+            <div className='container relative grid lg:grid-cols-3 lg:gap-7  md:items-center text-white justify-center' key={exercise.id}>
+              <h4 className='text-white text-center items-center uppercase'>{exercise.name}</h4>
+              <img className='container block opacity-100 transition-all  hover:opacity-30 lg:w-32 w-52 rounded-md   items-center justify-center' src={exercise.gifUrl} alt={exercise.bodyPart}/>
+              <div className='hover:opacity-100 absolute top-1/2 left-1/2 opacity-0 text-center items-center  -translate-x-1/2 -translate-y-1/2'> 
+                <button className='text-white w-28 rounded-sm border-solid ml-6 bg-slate-600 whitespace-nowrap opacity-80 'onClick={() => addMoveToMovelist(exercise)}>ADD MOVE </button> 
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
-  </div>
   </>
 )
 }
